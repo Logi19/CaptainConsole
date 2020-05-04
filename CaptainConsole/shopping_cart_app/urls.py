fb4cbdf07db7426d1cc025488ea3c4fe2576dc32
Logi Eyjólfsson <logi19@ru.is>
2020-05-04 17:04:46 +0000

    include() for cart urls, models implemented in store_app

--- CaptainConsole/shopping_cart_app/urls.py
+++ CaptainConsole/shopping_cart_app/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 
 from .views import ShoppingCartDetail
@@ -7,8 +6,7 @@ from .views import OrderDetail
 from .views import OrderCreate
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cart/', ShoppingCartDetail.as_view(), name='shopping_cart_detail'),
+    path('', ShoppingCartDetail.as_view(), name='shopping_cart_detail'),
     path('order/create/', OrderCreate.as_view(), name='order_create'),
     path('order/<pk>/', OrderDetail.as_view(), name='order_detail'),
     path('receipt/', ReceiptView.as_view(), name='receipt_view')
--- CaptainConsole/store_app/models.py
+++ CaptainConsole/store_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class Product(models.Model):
     """
-    Class for the store's products.
+    Django model for the store's products.
     """
     productName = models.CharField(max_length=256)
     productType = models.CharField(max_length=256)
@@ -11,25 +12,48 @@ class Product(models.Model):
     productDescription = models.TextField(blank=True)
 
     def get_images(self):
-        return ProductImages.objects.filter(productID=self.pk)
+        return ProductImages.objects.filter(product=self)
 
     def __str__(self):
         return self.productName
 
+
 class ProductImages(models.Model):
-    productID = models.ForeignKey("Product", on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_img', height_field=None, width_field=None, max_length=None)
+    """
+    Django model for the images of a product.
+    """
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    image = models.ImageField(
+        upload_to="product_img", height_field=None, width_field=None, max_length=None
+    )
+
 
 class Order(models.Model):
-    profileID = models.ForeignKey("profile_app.Profile", on_delete=models.CASCADE)
+    """
+    Django model for the orders placed in the store.
+    """
+    profile = models.ForeignKey("profile_app.Profile", on_delete=models.CASCADE)
+    processed = models.BooleanField(default=False)
     orderDiscount = models.DecimalField(max_digits=3, decimal_places=2)
     items = models.ManyToManyField("Product", through="OrderItem")
 
-    def get_total(self):
-        pass
+    def get_total_price(self):
+        """ Calculate and return total price of order, taking discounts into account. """
+        total = 0
+        for item in self.items:
+            item_price = item.product.productPrice - (
+                item.product.productPrice * (item.itemDiscount / 100)
+            )
+            total += item.quantity * item_price
+        total = total - (total * (self.orderDiscount / 100))
+        return total
+
 
 class OrderItem(models.Model):
-    orderID = models.ForeignKey("Order", on_delete=models.CASCADE)
-    productID = models.ForeignKey("Product", on_delete=models.CASCADE)
+    """
+    Django model for the items in an order.
+    """
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
     itemDiscount = models.DecimalField(max_digits=3, decimal_places=2)
     quantity = models.SmallIntegerField()
