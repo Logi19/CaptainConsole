1b5a73e881fb0c5c3a97d6bdce5c993f860f39c5
Guðmundur Már <gudmundurk18@ru.is>
2020-05-04 16:07:29 +0000

    views.py and urls.py for shopping_cart_app implemented

---
+++ CaptainConsole/shopping_cart_app/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import ShoppingCartDetail
+from .views import ReceiptView
+from .views import OrderDetail
+from .views import OrderCreate
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', ShoppingCartDetail.as_view(), name='shopping_cart_detail'),
+    path('order/create/', OrderCreate.as_view(), name='order_create'),
+    path('order/<pk>/', OrderDetail.as_view(), name='order_detail'),
+    path('receipt/', ReceiptView.as_view(), name='receipt_view')
+]
