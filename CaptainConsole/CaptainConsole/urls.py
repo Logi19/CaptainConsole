4f76a8cbfb24c4bb5837378e56544a9a3ca0b780
Logi Eyjólfsson <logi19@ru.is>
2020-05-07 19:45:58 +0000

    removed media root and added redirect

--- CaptainConsole/CaptainConsole/urls.py
+++ CaptainConsole/CaptainConsole/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url="sdmodel/", permanent=True), name='front_page_redirect'),
     path('admin/', admin.site.urls),
     path('cart/', include('shopping_cart_app.urls')),
     path('profile/', include('profile_app.urls')),
