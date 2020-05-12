9b31fc79711243baf2926661b14d866d3e1a24f1
Guðmundur Már <gudmundurk18@ru.is>
2020-05-12 20:57:13 +0000

    urls.py configured for removing an item from cart

--- CaptainConsole/shopping_cart_app/urls.py
+++ CaptainConsole/shopping_cart_app/urls.py
@@ -4,9 +4,11 @@ from .views import ShoppingCartDetail
 from .views import ReceiptView
 from .views import OrderDetail
 from .views import OrderCreate
+from .views import remove_from_cart
 
 urlpatterns = [
-    path('', ShoppingCartDetail.as_view(), name='shopping_cart_detail'),
+    path('<pk>/', ShoppingCartDetail.as_view(), name='shoppingcart_detail'),
+    path('delete/<product_id>/<cart_id>/', remove_from_cart, name='remove_from_cart'),
     path('order/create/', OrderCreate.as_view(), name='order_create'),
     path('order/<pk>/', OrderDetail.as_view(), name='order_detail'),
     path('receipt/', ReceiptView.as_view(), name='receipt_view')
