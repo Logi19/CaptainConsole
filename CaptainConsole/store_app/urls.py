356d63420a143cc49c975bbfbeb8a3cf68cfeba2
Logi Eyjólfsson <logi19@ru.is>
2020-05-13 20:42:25 +0000

    working on add to shopping cart js function

--- CaptainConsole/store_app/urls.py
+++ CaptainConsole/store_app/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import FrontPageView, check_out
 from .views import ProductList
 from .views import ProductDetail
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', FrontPageView.as_view(), name="frontpage"),
     path('products/', ProductList.as_view(), name="all_products"),
     path('products/<pk>/', ProductDetail.as_view(), name="product_detail"),
-    path('checkout/', views.check_out, name="checkout"),
+    path('checkout/', check_out, name="checkout"),
 ]
 
